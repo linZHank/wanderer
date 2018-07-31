adc0b702b4b6c41584713722022a7682ef0215bf
linzhank <linzhank@gmail.com>
2018-07-31 15:57:30 -0400

    correct turn angle

--- wanderer_navigation/scripts/go_home.py
+++ wanderer_navigation/scripts/go_home.py
@@ -10,10 +10,10 @@ from geometry_msgs.msg import Twist
 
 
 # Offset two wheels' speed by adding a term to angular speed in /cmd_vel
-WHEEL_OFFSET = 0.1 # increase right-wheel spinning
+WHEEL_OFFSET = 0. # increase right-wheel spinning
 
 def closeHome(position):
-  return np.linalg.norm(position) <= 0.4
+  return np.linalg.norm(position) <= 0.2
   
 class WandererDriver():
   """
@@ -25,6 +25,7 @@ class WandererDriver():
     self.y = np.inf
     self.alpha = 0
     self.beta = 0
+    self.turn = 0
     self.cmd = Twist()
     self.stop_cmd = Twist()
     self.stop_cmd.linear.x = 0
@@ -45,8 +46,10 @@ class WandererDriver():
     ]
     rpy = tf.transformations.euler_from_quaternion(quat)
     self.alpha = rpy[2] # approximate to camera_link's planar orientation
-    self.beta = math.atan2(-self.x, -self.y) # angular difference from map origin to wanderer
+    self.beta = math.atan2(self.y, self.x) # angular difference from map origin to wanderer
     print("x={:.3f}, y={:.3f}, aplha={:.3f}".format(self.x, self.y, self.alpha))
+    self.turn = -math.atan2(-math.sin(self.alpha-self.beta), -math.cos(self.alpha-self.beta))
+    print("turn your head {}".format(self.turn))
 
   def go_home(self):
     """
@@ -56,15 +59,19 @@ class WandererDriver():
     while not rospy.is_shutdown():
       #self.sub_campose()
       if not closeHome([self.x, self.y]):
-        if math.fabs(self.beta-self.alpha) > 0.3:  # first aims at home          
-          angular = self.beta - self.alpha + WHEEL_OFFSET #
-          if angular > 0.6:
-            angular = 0.6
-          elif angular < -0.6:
-            angular = -0.6
+        if math.fabs(self.turn) > 0.2:  # first aims at home          
+          angular = self.turn + WHEEL_OFFSET #
+          if angular > 0.1:
+            angular = 0.1 + WHEEL_OFFSET
+          elif angular < -0.1:
+            angular = -0.1 + WHEEL_OFFSET
           linear = 0
         else:  # then move straight forward
           linear = math.sqrt(self.x ** 2 + self.y ** 2)
+          if linear > 0.2:
+            linear = 0.2
+          elif linear < -0.2:
+            linear = -0.2
           angular = 0 + WHEEL_OFFSET
         self.cmd.linear.x = linear
         self.cmd.angular.z = angular
